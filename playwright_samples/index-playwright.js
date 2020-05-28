c966f5ffd3ca49f400b66a49c912b4cb3933e394
Raha Mohebbi <rahamohebbi@gmail.com>
2020-05-27 21:11:52 -0700

    clean up

--- playwright_samples/index-playwright.js
+++ playwright_samples/index-playwright.js
@@ -3,7 +3,7 @@ const pw = require('playwright');
 (async () => {
     const browser = await pw.chromium.connect({
         wsEndpoint:
-            'wss://proxy.0browser.com?token=5299f72f-f05b-4589-ad5d-1e9b3de55b9d&timeout=60000'
+            'wss://proxy.0browser.com?token={YOUR-API-KEY}&timeout=60000'
     });
     const context = await browser.newContext({ acceptDownloads: true });
     const page = await context.newPage({ acceptDownloads: true });
